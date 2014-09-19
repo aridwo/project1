951e8d2d6f032e0dbb0aa9a5fc2c778491f3b0d2
Arielle Dworkin <arielle_dworkin@hks.harvard.edu>
2014-09-18 22:54:56 -0400

    updated the homepage with the required content

--- index.php
+++ index.php
@@ -1,1 +1,39 @@
-test -- howdy!
+<!DOCTYpE html pUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Arielle Dworkin - Project 1</title>
+<style type="text/css">
+@import url(http://fonts.googleapis.com/css?family=Montserrat:400,700);
+@import url(http://fonts.googleapis.com/css?family=Pacifico);
+p{font-family: 'Montserrat', sans-serif;}
+h2 {font-family: 'Pacifico', cursive;color:orange;)
+a {text-decoration: none; color:orange;}
+
+
+
+</style>
+</head>
+
+<body><span style="text-align: center">
+   <h2>Arielle Dworkin    </h2>
+   <p><img src="https://media.licdn.com/media/p/3/000/115/3f4/0054eb3.jpg" width="253" height="253" /></p>
+   <p>Project 2 Title<br>
+View: [<a href="http://p2.aridwo.me">link</a>] Github: [link]</p>
+<p>Project 3 Title<br>
+View: [<a href="http://p3.aridwo.me">link</a>] 		Github: [link]</p>
+<p>Project 4 Title<br>
+View: [<a href="http://p4.aridwo.me">link</a>] 		Github: [link]</p></span>
+<p><strong>In 2+ sentences describe who you are and what you do.</strong></p>
+<p>I am currently a Graduate Student in Information Technology, with a concentration in Digital media arts and instructional design at Harvard University's Extension School.   </p>
+   <p><strong>Why are you taking this course?   </strong></p>
+   <p>I am taking this course to further my web programming and backend development skills. </p>
+   <p><strong>What previous experience do you have with programming?</strong></p>
+   <p>My only prior experience with programming has been in the classroom -- I have completed the Harvard courses Introduction to Computer Science and Creative Explorations in Screen-Based and physical Computing.</p>
+<p><strong>Are you working on a Mac, PC, or some other platform?</strong></p>
+<p>Mac.</p>
+<p><strong>Anything else we should know?</strong></p>
+<p>When I'm not immersed in all things digital I enjoy traveling, running, the beach, photography, and my two daschunds. </p>
+</body>
+</html>
